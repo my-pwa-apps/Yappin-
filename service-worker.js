c3d591a8ee0a96f3784d85b7ee1017d8c0f01603
GarfieldApp <garfieldapp@outlook.com>
2025-05-10 18:44:09 +0200

    refactor: remove unused image assets from service worker cache

--- service-worker.js
+++ service-worker.js
@@ -9,15 +9,6 @@ const urlsToCache = [
   '/js/auth.js',
   '/js/firebase-config.js',
   '/js/timeline.js',
-  '/images/icons/icon-72x72.png',
-  '/images/icons/icon-96x96.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-144x144.png',
-  '/images/icons/icon-152x152.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-384x384.png',
-  '/images/icons/icon-512x512.png',
-  '/images/default-avatar.png',
   '/manifest.json'
 ];
 
